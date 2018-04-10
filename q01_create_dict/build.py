a41295ba407fa817363e062f3e0fbc80a4679c47
PBPatil <theprasadpatil@gmail.com>
2018-04-10 10:51:10 +0000

    Done

--- q01_create_dict/build.py
+++ q01_create_dict/build.py
@@ -1,8 +1,30 @@
+# %load q01_create_dict/build.py
 import pandas as pd
 
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q01_create_dict():
-    "write your solution here"
+def q01_create_dict(data):
+    count = data.Name.value_counts()
+    names = count.index
+    name_count = count.values
+    keys = list(names)
+    vals = list(name_count)
+    return dict(zip(keys,vals))
+    
+
+        
+    
+   
+    'write your solution here'
+
+
+
+
+
+
+
+
+
+
 
